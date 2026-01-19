aeffb21d660cb0b586fd87a544a86e1758f8e96e
ALPHA2204 <thisisalok1334@gmail.com>
2026-01-19 11:16:41 +0530

    Rate Limiter Settings

--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
 import express  from "express";
 import cors from "cors";
-import { psqlconnect } from "./db/db.psqlconnect.js";
+import ratelimit from "express-rate-limit"
+import swaggerUi from "swagger-ui-express"
+import axios from "axios"
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import { error } from "winston";
+
 
 const app = express()
 
-psqlconnect()
+app.use(cors({
+    origin:["http://localhost:3000"],
+    allowedHeaders:["Content-Type","Authorization"],
+    credentials:true
+}))
+
+app.use(morgan("dev"))
+app.use(express.json({limit:"100mb"}));
+app.use(express.urlencoded({limit:"100mb",extended:true}));
+app.use(cookieParser());
+
+const limiter = ratelimit({
+    windowMs:15*60*1000,//15 minu
+
+    max:(req)=>(req.user?1000:100), // for login user they will give the 1000 req and for not logined user 100 request
+    message:{error: "Too many requests from this IP, please try again after 15 minutes"},
+    standardHeaders:true,
+    legacyHeaders:true,
+    keyGenerator:(req)=>{
+        return req.ip
+    }
+
+})
+
+app.use(limiter)
+
+
+
 app.get("/api",(req,res)=>{
     res.send({message:`Welcome to SparkOn`})
 })
